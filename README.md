9a8d8b77e177bf24de26976324b87de249a85ae7
jessicamdsn <jessicasnevess@gmail.com>
2023-06-02 15:06:10 -0300

    add project about

---
+++ README.md
@@ -0,0 +1,14 @@
+<p align="center">
+<img src=".github/preview.png" alt="demonstração do projeto" width="100%"/>
+</p>
+
+## 🖥️ Projeto
+Esse é um projeto Web Responsivo para fazer cálculos simples
+
+## 🚀 Tecnologias
+Esse projeto foi desenvolvido durante os estudos com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+- Git e Github
---
+++ javascript.js
@@ -0,0 +1,30 @@
+
+function somar() {
+    var tn1 = window.document.getElementById('txtn1')
+    var tn2 = window.document.getElementById('txtn2')
+    var res = window.document.getElementById('Sres')
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var s = n1 + n2
+    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong> ${s} </strong>`
+}
+
+function multiplicar() {
+    var tn1 = window.document.getElementById('m1')
+    var tn2 = window.document.getElementById('m2')
+    var res = window.document.getElementById('Mres')
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var m = n1 * n2
+    res.innerHTML = `A Multiplicação entre ${n1} e ${n2} é igual a <strong> ${m} </strong>`
+}
+
+function dividir() {
+    var tn1 = window.document.getElementById('d1')
+    var tn2 = window.document.getElementById('d2')
+    var res = window.document.getElementById('Dres')
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var d = n1 / n2
+    res.innerHTML = `A divisão entre ${n1} e ${n2} é igual a <strong> ${d} </strong>`
+}
